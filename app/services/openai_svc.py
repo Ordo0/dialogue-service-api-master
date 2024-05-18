7a3c2696c70e557f778b16ec917829e602ec8dae
Ordo <6575011@gmail.com>
2024-05-18 23:13:34 +0300

    Initial commit

---
+++ app/services/openai_svc.py
@@ -0,0 +1,12 @@
+import time
+
+
+def generate_openai_prompt(prompt: str) -> str:
+    """
+    Simple client for OpenAi Api request
+    """
+    # with requests.Session() as session:
+    #         },
+
+    time.sleep(10)
+    return "test_77"
